359d98e4246bcd372f9d0ec651ab615033ebbbcf
Vitor <moutimg@gmail.com>
2022-03-04 02:04:46 -0300

    Da acesso a store atraves do Provider

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import Login from './pages/Login';
 import RedirectPage from './pages/RedirectPage';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route path="/redirect" component={ RedirectPage }/>
-        <Route path="/profile" component={ Profile } />
-        <Route path="*" component={ NotFound } />
-      </Switch>
+      <Provider store={ store }>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/redirect" component={ RedirectPage }/>
+          <Route path="/profile" component={ Profile } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </Provider>
     )
   }
 }
