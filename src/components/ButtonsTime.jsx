79df0118862e1f85843615b16210c608debd699b
Vitor <moutimg@gmail.com>
2022-03-04 20:01:04 -0300

    Parcial botoes de periodo

---
+++ src/components/ButtonsTime.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ButtonsTime extends Component {
+  render() {
+    return (
+      <div>ButtonsTime</div>
+    )
+  }
+}
+
+export default ButtonsTime
